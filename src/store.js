f080607542ffb9cad85659cd4e88209d0bb4c016
BVNguyenAn <anbuiuytin@gmail.com>
2023-09-13 23:48:30 +0700

    upload

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { legacy_createStore as createStore  } from "redux";
+import { combineReducers, applyMiddleware  } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import TodoReducer from "./reducer/TodoReducer";
+
+const reducer = combineReducers({
+    Todo:TodoReducer,
+});
+
+ const loadState = () => {
+    try {
+      const stateString = localStorage.getItem("todos");
+      return JSON.parse(stateString);
+    } catch (error) {
+      return undefined;
+    }
+  };
+
+const persistedState = loadState();
+console.log(persistedState);
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    persistedState || initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
